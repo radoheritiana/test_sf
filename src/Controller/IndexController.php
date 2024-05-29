fd56360a324b96ccc01a53dc946459683cd08ea1
radoheritiana <radoheritiana.rhja.123@gmail.com>
2024-05-29 09:30:59 +0300

    create player and list player in index

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Player;
 use App\Entity\Team;
+use App\Form\PlayerFormType;
 use App\Form\TeamFormType;
 use App\Repository\PlayerRepository;
 use App\Repository\TeamRepository;
@@ -26,6 +28,8 @@ class IndexController extends AbstractController
     #[Route('/', name: 'app_index')]
     public function index(Request $request, PaginatorInterface $paginator, EntityManagerInterface $em): Response
     {
+
+        //team pagination
         $dql   = "SELECT t FROM App\Entity\Team t";
         $query = $em->createQuery($dql);
 
@@ -35,14 +39,26 @@ class IndexController extends AbstractController
             5
         );
 
+        //player pagination
+        $dql   = "SELECT p FROM App\Entity\Player p";
+        $query = $em->createQuery($dql);
+
+        $paginate_players = $paginator->paginate(
+            $query,
+            $request->query->getInt('page', 1),
+            5
+        );
+
         return $this->render('index.html.twig', [
-            'all_teams' => $paginate_teams
+            'all_teams' => $paginate_teams,
+            'all_players' => $paginate_players
         ]);
     }
 
     #[Route('/add-team-player', name: 'app_create_team')]
     public function add_team_and_player(Request $request, EntityManagerInterface $em): Response
     {
+        // handle create team
         $team = new Team();
         $team_form = $this->createForm(TeamFormType::class, $team);
         $team_form->handleRequest($request);
@@ -52,8 +68,21 @@ class IndexController extends AbstractController
             $em->flush();
             return $this->redirectToRoute('app_index');
         }
+
+        //handle create player
+        $player = new Player();
+        $player_form = $this->createForm(PlayerFormType::class, $player);
+        $player_form->handleRequest($request);
+
+        if ($player_form->isSubmitted() && $player_form->isValid()) {
+            $em->persist($player);
+            $em->flush();
+            return $this->redirectToRoute('app_index');
+        }
+
         return $this->render('add_team_and_player.html.twig', [
-            'team_form' => $team_form
+            'team_form' => $team_form,
+            'player_form' => $player_form
         ]);
     }
 
---
+++ src/Form/PlayerFormType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Player;
+use App\Entity\Team;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PlayerFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstName', TextType::class, [
+
+            ])
+            ->add('lastName', TextType::class, [
+
+            ])
+            ->add('team', EntityType::class, [
+                'class' => Team::class,
+                'choice_label' => 'name',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Player::class,
+        ]);
+    }
+}
